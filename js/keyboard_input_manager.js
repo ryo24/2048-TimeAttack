1439181c08a2bc4a62fba1d0bc06a69c1effcb80
Hiroaki Hosono <hosono_hiroaki@applibot.co.jp>
2014-04-17 00:41:33 +0900

    key config

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -31,23 +31,24 @@ KeyboardInputManager.prototype.emit = function (event, data) {
   }
 };
 
+var keyMap = {
+  38: 0, // Up
+  39: 1, // Right
+  40: 2, // Down
+  37: 3, // Left
+  75: 0, // Vim up
+  76: 1, // Vim right
+  74: 2, // Vim down
+  72: 3, // Vim left
+  87: 0, // W
+  68: 1, // D
+  83: 2, // S
+  65: 3  // A
+};
+
 KeyboardInputManager.prototype.listen = function () {
   var self = this;
 
-  var map = {
-    38: 0, // Up
-    39: 1, // Right
-    40: 2, // Down
-    37: 3, // Left
-    75: 0, // Vim up
-    76: 1, // Vim right
-    74: 2, // Vim down
-    72: 3, // Vim left
-    87: 0, // W
-    68: 1, // D
-    83: 2, // S
-    65: 3  // A
-  };
 
   // Respond to direction keys
   document.addEventListener("keydown", function (event) {
